e43b541b7de5d4e98940530ecd1e7244cbed8ec0
mcivase <kerenivase.com>
2025-05-21 12:12:41 -0400

    create budget function

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-git_remote_add
+monthly_budget = 3000
+budget_categories = ["Housing", "Transportation", "Groceries", "Debt", "Savings & investments", "Selfcare", "Entertainment", "Miscellaneous"]
+# make category function
+def budget_category():
+    pass
