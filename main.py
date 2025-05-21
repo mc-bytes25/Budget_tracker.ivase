9e04fc652f2102ec386942debffaac206444ae12
mcivase <kerenivase.com>
2025-05-21 13:04:06 -0400

    print categories for budget

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 monthly_budget = 3000
 budget_categories = ["Housing", "Transportation", "Groceries", "Debt", "Savings & investments", "Selfcare", "Entertainment", "Miscellaneous"]
 # make category function
-def budget_category():
-    pass
+def list_category():
+    for name in budget_categories:
+        print(name)
+# list_category() 
+   
